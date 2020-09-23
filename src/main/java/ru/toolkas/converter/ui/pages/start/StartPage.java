2ec39973eac9abbcd8bc7f88a71f1987445ad907
tulkas <rogovdn.asu.2@gmail.com>
2020-09-23 21:21:08 +0300

    UI Styles

--- src/main/java/ru/toolkas/converter/ui/pages/start/StartPage.java
+++ src/main/java/ru/toolkas/converter/ui/pages/start/StartPage.java
@@ -2,6 +2,7 @@ package ru.toolkas.converter.ui.pages.start;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import ru.toolkas.converter.domain.Valute;
@@ -12,6 +13,7 @@ import ru.toolkas.converter.ui.util.ValuteRenderer;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -37,8 +39,8 @@ public class StartPage extends BasePage {
             List<Valute> valutes = new ArrayList<>(currencyConversionService.getTodayValutes());
             valutes.sort(Comparator.comparing(Valute::getCharCode));
 
-            add(new DropDownChoice<>("fromCurrency", new PropertyModel<Valute>(this, "from"), valutes, new ValuteRenderer()));
-            add(new DropDownChoice<>("toCurrency", new PropertyModel<Valute>(this, "to"), valutes, new ValuteRenderer()));
+            add(new ValuteChoice("fromCurrency", new PropertyModel<Valute>(this, "from"), valutes, new ValuteRenderer()));
+            add(new ValuteChoice("toCurrency", new PropertyModel<Valute>(this, "to"), valutes, new ValuteRenderer()));
             add(new NumberTextField<BigDecimal>("fromAmount", new PropertyModel<>(this, "fromAmount")));
 
             add(new Label("result", new PropertyModel<>(this, "result")));
@@ -49,7 +51,20 @@ public class StartPage extends BasePage {
             super.onSubmit();
 
             BigDecimal toAmount = currencyConversionService.convert(from, fromAmount, to);
-            result = new DecimalFormat("##").format(fromAmount) + " " + from.getCharCode() + " = " + new DecimalFormat("##.0000").format(toAmount) + " " + to.getCharCode();
+            result = fromAmount + " " + from.getCharCode() + " = " + toAmount.setScale(4, RoundingMode.HALF_UP) + " " + to.getCharCode();
+        }
+    }
+
+    private static class ValuteChoice extends DropDownChoice<Valute> {
+        private static final long serialVersionUID = 8091192321910887352L;
+
+        public ValuteChoice(String id, IModel<Valute> model, List<? extends Valute> choices, IChoiceRenderer<? super Valute> renderer) {
+            super(id, model, choices, renderer);
+        }
+
+        @Override
+        protected String getNullKeyDisplayValue() {
+            return "Валюта";
         }
     }
 }
